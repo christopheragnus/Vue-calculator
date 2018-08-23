36121cfc383839cc6af7ff37541835c396a8b7f1
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-08-24 00:25:01 +1000

    complete calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,58 +1,121 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="calculator">
+    <div class="display">{{current || '0'}}</div>
+    <div @click="clear" class="btn" >C</div>
+    <div @click="sign" class="btn" >+/-</div>
+    <div @click="percent" class="btn" >%</div>
+    <div @click="divide" class="btn operator" >÷</div>
+    <div @click="append('7')" class="btn" >7</div>
+    <div @click="append('8')" class="btn" >8</div>
+    <div @click="append('9')" class="btn" >9</div>
+    <div @click="times" class="btn operator" >x</div>
+    <div @click="append('4')" class="btn" >4</div>
+    <div @click="append('5')" class="btn" >5</div>
+    <div @click="append('6')" class="btn" >6</div>
+    <div @click="subtract" class="btn operator" >-</div>
+    <div @click="append('1')" class="btn" >1</div>
+    <div @click="append('2')" class="btn" >2</div>
+    <div @click="append('3')" class="btn" >3</div>
+    <div @click="add" class="btn operator" >+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn" >.</div>
+    <div class="bt operator" >=</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+ data() {
+   return {
+     current: '',
+     operator: null,
+     previous: null,
+     operatorClicked: false
+   }
+ },
+ // to define a function on your Vue component
+ methods: {
+   clear() {
+     this.current = '';
+   },
+   sign() {
+     this.current = this.current.charAt(0) === '-' ?
+      this.current.slice(1) : `-${this.current}`;
+   },
+   percent() {
+     this.current = `${parseFloat(this.current) / 100}`
+   },
+   append(number) {
+     if (this.operatorClicked) {
+       this.current = '';
+       this.operatorClicked = false;
+     }
+     this.current = `${this.current}${number}`;
+   },
+   dot(){
+     if (this.current.indexOf('.') === -1) {
+       this.append('.');
+     }
+   },
+   setPrevious() {
+     this.previous = this.current;
+     this.operatorClicked = true;
+   },
+   divide() {
+     this.operator = (a,b) => a / b;
+     this.setPrevious;
+   },
+   times() {
+     this.operator = (a, b) => a * b;
+     this.setPrevious;
+   },
+   add() {
+     this.operator = (a, b) => a + b;
+     this.setPrevious;
+   },
+   subtract() {
+     this.operator = (a, b) => a - b;
+     this.setPrevious;
+   },
+   equal() {
+     this.current = `${this.operator(
+       parseFloat(this.current), 
+       parseFloat(this.previous)
+      )}`;
+      this.previous = null;
+   }
+ }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.calculator {
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr); 
+  grid-auto-rows:minmax(50px, auto);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.zero {
+  grid-column: 1 / 3;
 }
-a {
-  color: #42b983;
+
+.btn {
+  background-color: #eee;
+  border: 1px solid #999;
+}
+
+.operator {
+  background-color: orange;
+
 }
 </style>
